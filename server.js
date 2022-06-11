e0d463a8dcf71d41f29de27abc2bd1ef1f5f6fb1
Ashwani Ranjan <ranjan062001@gmail.com>
2022-06-11 08:57:22 +0530

    pushing 4th time on heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const db = require("./config/db");
 const cors = require("cors");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 const userRoutes = require("./routes/userRoutes");
 const evidenceRoutes = require("./routes/evidenceRoute");
@@ -11,19 +12,27 @@ app.use(express.json());
 app.use(cors());
 db();
 
-app.get("/", (req, res) => {
-    res.end("Its running");
-});
+// app.get("/", (req, res) => {
+//     res.end("Its running");
+// });
 
-app.get("/another", (req, res) => {
-    res.end("Its running here also");
-});
+// app.get("/another", (req, res) => {
+//     res.end("Its running here also");
+// });
 
 app.use("/api/public/", express.static("public/evidence"));
 app.use("/api/public/audios", express.static("public/audios"));
 app.use("/api/user", userRoutes);
 app.use("/api/evidence", evidenceRoutes);
 app.use("/api/contact", contactRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on : http://localhost:${PORT}`);
