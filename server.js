5448b8938bab2c47636c1afda67842ff5c48b734
Ashwani Ranjan <ranjan062001@gmail.com>
2022-06-11 09:20:28 +0530

    pushing 5th time

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ app.use("/api/user", userRoutes);
 app.use("/api/evidence", evidenceRoutes);
 app.use("/api/contact", contactRoutes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//     });
+// }
 
 app.listen(PORT, () => {
     console.log(`Server is running on : http://localhost:${PORT}`);
